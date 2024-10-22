f58e79ee2798e299fd90a4d7f9dad8393edc482c
unknown <anirudhkuppili@gmail.com>
2024-10-22 09:33:05 -0500

    Navbar adjust #7

--- client/src/app/components/login.jsx
+++ client/src/app/components/login.jsx
@@ -7,7 +7,7 @@ export const Login = () => {
     const { setDialogOpen, connected, setConnected, setBalance } = useContext(DialogContext);
 
     return (
-        <Chip label={ connected === true ? "Logout" : "Login"} className="chip" style={{ marginRight: 'auto', alignSelf: 'center' }} onClick={() => {
+        <Chip label={ connected === true ? "Logout" : "Login"} className="chip" style={{ marginRight: 'auto', marginTop: '25%' }} onClick={() => {
             if (connected) {
                 setConnected(false);
                 setBalance(null);
